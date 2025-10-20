d047adf9a8199a52c070b77bcfba589c929e0f83
Jack LUC <jackluc@MacBook-Air-de-Jack.local>
2025-10-20 18:36:28 +0200

    fix: base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 
 export default defineConfig({
+  base: '/MY-FIRE-DEAL-HOLDING/',
   plugins: [react()],
   resolve: {
     alias: {
